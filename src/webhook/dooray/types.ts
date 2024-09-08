9a81626541aeb885d7cdbf1d83a11ff236c18daa
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-08 18:34:00 +0900

    chore: add dooray webhook attachments

--- src/webhook/dooray/types.ts
+++ src/webhook/dooray/types.ts
@@ -1,4 +1,5 @@
 // Reference https://helpdesk.dooray.com/share/pages/9wWo-xwiR66BO5LGshgVTg/2900079668515933969
+// Reference https://helpdesk.dooray.com/share/pages/9wWo-xwiR66BO5LGshgVTg/2900079844453730084
 
 export type Options = {
 	url: `https://hook.dooray.com/services/${string}`;
@@ -9,4 +10,12 @@ export type RequestBody = {
 	botName: string;
 	botIconImage?: `https://${string}`;
 	text: string;
+	attachments?: Array<
+		Partial<{
+			title: string;
+			titleLink: `https://${string}`;
+			text: string;
+			color: string;
+		}>
+	>;
 };
