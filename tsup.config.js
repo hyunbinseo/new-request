e5c8e5cefce41851ae7f1706ea0c2a2528bf7746
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-08 18:06:24 +0900

    chore: bump dependencies, node, pnpm

--- tsup.config.js
+++ tsup.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'tsup';
 
+// TODO Keep directory structure on build output.
+// Reference https://github.com/egoist/tsup/issues/728
+
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['cjs', 'esm'],
