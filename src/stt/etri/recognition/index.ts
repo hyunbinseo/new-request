49a4a1caa2ea05639c71bb8a1bec7defcd24075e
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-04-17 22:53:15 +0900

    feat: etri speech recognition api

---
+++ src/stt/etri/recognition/index.ts
@@ -0,0 +1,30 @@
+import type { Options, RequestBody, ResponseBody200, ResponseError } from './types.js';
+export type { Options, RequestBody };
+
+export const speechToText = async (requestBody: RequestBody, opts: Options) => {
+	const request = new Request('http://aiopen.etri.re.kr:8000/WiseASR/Recognition', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json', // required
+			'Authorization': opts.accessKey,
+		},
+		body: JSON.stringify(requestBody),
+	});
+
+	try {
+		const response = await (opts.fetch || fetch)(request);
+		return response.ok
+			? {
+					ok: response.ok,
+					status: response.status as 200,
+					body: (await response.json()) as ResponseBody200,
+			  }
+			: ({
+					ok: response.ok,
+					status: response.status,
+					body: await response.json(),
+			  } as ResponseError);
+	} catch (error) {
+		return error instanceof Error ? error : new Error();
+	}
+};
---
+++ src/stt/etri/recognition/types.ts
@@ -0,0 +1,81 @@
+// Reference https://aiopen.etri.re.kr/guide/Recognition
+
+export type Options = {
+	accessKey: string;
+	fetch?: typeof fetch;
+};
+
+export type LanguageCode =
+	| 'korean'
+	| 'english'
+	| 'japanese'
+	| 'chinese'
+	| 'spanish'
+	| 'french'
+	| 'german'
+	| 'russian'
+	| 'vietnam'
+	| 'arabic'
+	| 'thailand'
+	| 'portuguese'
+	| 'malaysia'
+	| 'indonesian'
+	| 'cantonese'
+	| 'nederlands';
+
+export type RequestBody = {
+	argument: {
+		language_code: LanguageCode;
+		audio: string;
+	};
+};
+
+export type ResponseBody200 = {
+	request_id: string;
+	result: 0;
+	return_object: { recognized: string };
+};
+
+// TODO Check response status value and body type for this failure case:
+// 음성인식이 제대로 이루어지지 않았을 경우에는 음성인식 결과 대신 오류 메시지가 반환됩니다.
+
+type ErrorReason = {
+	400:
+		| 'ASR_NOTOKEN' // 파일이 요구사항(샘플링 주파수 16kHz)에 맞지 않을 때
+		| 'SayTranError NoToken'
+		| 'SayTranError NoSpeech'
+		| 'SayTranError NetworkError'
+		| 'SayTranError NetworkError - SR data block broken'
+		| 'SayTranError NetworkError - BufferOverflow'
+		| 'SayTranError InputError - sr unknown LANG'
+		| 'SayTranError InputError - sr header mismatchr'
+		| 'SayTranError EngineBusy - sr engine full';
+	403:
+		| 'Empty Auth Header'
+		| 'Invalid Key'
+		| 'Blocked KEY'
+		| 'Daily Limit Exceeded'
+		| 'Monthly Limit Exceeded'
+		| 'Yearly Limit Exceeded'
+		| 'Too Many Keys'
+		| 'Too Many IPs'
+		| 'Not Allowed IP'
+		| 'Not Allowed Subpath'
+		| 'Invalid API';
+	408: 'Request Timeout';
+	413: 'Body Size Limit Exceeded';
+	429: 'Concurrent Limit Exceeded';
+	500: 'Internal Server Error';
+};
+
+type ErrorStatus = keyof ErrorReason;
+
+export type ResponseError<S extends ErrorStatus = ErrorStatus> = {
+	ok: false;
+	status: S;
+	body: {
+		request_id: string;
+		result: -1;
+		reason: ErrorReason[S];
+	};
+};
