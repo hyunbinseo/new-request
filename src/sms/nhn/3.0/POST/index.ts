1b67fea7b26ec04c0a13a5b4dc8ac9c7fa2a23c7
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-04-28 10:12:04 +0900

    chore!: remove legacy exports

--- src/sms/nhn/3.0/POST/index.ts
+++
@@ -1,5 +0,0 @@
-// TODO Remove this export in a major version update.
-
-export type { Options, RequestBody } from '../../v3.0/POST/types.ts';
-
-export { sendSms } from '../../v3.0/POST/index.ts';
--- src/webhook/dooray/index.ts
+++
@@ -1,5 +0,0 @@
-// TODO Remove this export in a major version update.
-
-export type { Options, RequestBody } from '../../message/dooray/types.ts';
-
-export { sendMessage } from '../../message/dooray/index.ts';
